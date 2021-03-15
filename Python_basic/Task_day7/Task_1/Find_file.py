2dd3fda05a7d41e6edb97477fc17c09a0925697c
Prin Meng <772062725@qq.com>
2021-03-15 17:53:54 +0800

    7 days

--- Python_basic/Task_day7/Task_1/Find_file.py
+++ Python_basic/Task_day7/Task_1/Find_file.py
@@ -10,8 +10,8 @@ print('方案一：')
 for file_or_dir in file_dir_list:
     if os.path.isfile(file_or_dir):
         test_file = open(file_or_dir)
-        for lines in test_file.readlines():
-            if re.findall('qytang', lines):
+        for line in test_file.readlines():
+            if re.findall('qytang', line):
                 print(file_or_dir)
                 break
 
@@ -22,12 +22,12 @@ for root, dirs, files in os.walk(os.getcwd(), topdown=False):
     for name in files:
         if name != '':
             test_file1 = open(name)
-            for lines in test_file1.readlines():
-                if re.findall('qytang', lines):
+            for line in test_file1.readlines():
+                if re.findall('qytang', line):
                     print(name)
                     break
 
-# 清理创建测试文件的
+# 清理创建的测试文件
 os.chdir('..')
 
 for root, dirs, files in os.walk('test', topdown=False):
