87b88460785a0aba7e495552e9f1818023a82ef4
Prin Meng <772062725@qq.com>
2021-03-16 13:27:52 +0800

    8 days

--- Python_basic/Task_day8/Task_2.py
+++ Python_basic/Task_day8/Task_2.py
@@ -4,8 +4,21 @@ list2 = ['bbb', 333, 111, 3.14, (4, 5)]
 print('方案一：')
 
 for item1 in list1:
-    for item2 in list2:
-        if item2 == item1:
-            print(str(item1) + ' only in list1 and list2')
+    if item1 in list2:
+        print(str(item1) + ' in List1 and List2')
+    else:
+        print(str(item1) + ' only in List1')
 
-print('方案二：')
+print('\n方案二：')
+
+
+def find_same(l1, l2):
+    for item1 in list1:
+        if item1 in list2:
+            print(str(item1) + ' in List1 and List2')
+        else:
+            print(str(item1) + ' only in List1')
+
+
+if __name__ == '__main__':
+    find_same(list1, list2)
--- Python_basic/Task_day8/Task_3.py
+++ Python_basic/Task_day8/Task_3.py
@@ -1,24 +1,18 @@
 import logging
-from kamene.layers.inet import IP, ICMP
-from kamene.sendrecv import sr1
-
 logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
+from kamene.all import *
 
 
 def qytang_ping(ip):
     ping_pkt = IP(dst=ip) / ICMP()
     ping_result = sr1(ping_pkt, timeout=2, verbose=False)
     if ping_result:
-        return 'ip 通！'
+        return str(ip) + ' 通！'
     else:
-        return False
+        return str(ip) + ' 不通！'
+
 
 if __name__ == '__main__':
     result = qytang_ping('192.168.0.1')
-    if result:
-        print(result)
-    else:
-        print('此地址不通')
-
-
+    print(result)
 
