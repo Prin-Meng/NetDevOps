6bb61e518d7618cade2007070b7cac9cde0bf7d9
Prin Meng <772062725@qq.com>
2021-03-18 13:00:27 +0800

    10 days

--- Python_basic/Task_day10/Task_1/collect_if_info.py
+++ Python_basic/Task_day10/Task_1/collect_if_info.py
@@ -2,18 +2,16 @@ from qytang_ssh import qytang_ssh
 from qytang_ping import qytang_ping
 import re
 import pprint
-import getpass
 
 
-def qytang_get_if(*ips, username='admin', password='huawei@123'):
+def qytang_get_if(*ips, username='admin', password='Huawei@123'):
     device_if_dict = {}
     for ip in ips:
         result = qytang_ping(ip)
         if '不通' not in result:
-            if_str = qytang_ssh(ip, username, password, port=22, cmd='display ip interface brief\n')
-            if_info = re.findall('([A-Z].+\d)\s+\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}', if_str)
-            ip_info = re.findall('[A-Z].+\d\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})', if_str)
-            if_dict = dict(zip(if_info, ip_info))
+            if_str = qytang_ssh(ip, username, password, 22, 'display ip interface brief\n')
+            if_info = re.findall('([A-Z].+\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})', if_str)
+            if_dict = dict(if_info)
         else:
             if_dict = {}
         new_dict = {ip: if_dict}
