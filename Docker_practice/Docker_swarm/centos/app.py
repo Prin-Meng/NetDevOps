ff00b87d524c295f42549209367db4f06785da12
Prin Meng <772062725@qq.com>
2021-04-28 22:09:52 +0800

    docker swarm practice

---
+++ Docker_practice/Docker_swarm/centos/app.py
@@ -0,0 +1,9 @@
+import time
+
+i = 0
+
+while True:
+    i += 1
+    print(i, flush=True)
+    time.sleep(1)
+
--- Docker_practice/Docker_swarm/flask/app.py
+++ Docker_practice/Docker_swarm/flask/app.py
@@ -1,15 +1,12 @@
 import os
 import re
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 
 def return_hostname():
-    ifconfig_result = os.popen('ifconfig ' + 'ens33').read()
-
+    ifconfig_result = os.popen('ifconfig ' + 'eth0').read()
     # 正则表达式查找ip
     ipv4_add = re.findall('inet\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', ifconfig_result)[0]
     return ipv4_add
@@ -18,7 +15,7 @@ def return_hostname():
 @app.route('/')
 def hello():
     host = return_hostname()
-    return f'Hello world version 1.0, I am doge.\n'
+    return f'Hello world version 1.0, I am {host}\n'
 
 
 if __name__ == '__main__':
