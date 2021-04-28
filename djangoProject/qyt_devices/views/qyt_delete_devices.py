cee6567c128dd7dfa797a17db150359f5c7d7973
Prin Meng <772062725@qq.com>
2021-04-28 12:41:57 +0800

    django_login

--- djangoProject/qyt_devices/views/qyt_delete_devices.py
+++ djangoProject/qyt_devices/views/qyt_delete_devices.py
@@ -1,8 +1,17 @@
 from qyt_devices.views.qyt_show_devices import show_devices
 from qyt_devices.models import Devicedb
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 def delete_devices(request, device_id):
+    # 没有登录
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/accounts/login?next=/show_devices')
+    # 登录但没有权限
+    elif not request.session.get('device_delete_permission') and request.user.is_authenticated:
+        errormessage = '您无权进行此操作'
+        return render(request, 'permission_deny.html', {'errormessage': errormessage})
+
     try:
         # 获取对应ID的设备的条目
         del_device = Devicedb.objects.get(id=device_id)
