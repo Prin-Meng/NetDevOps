b0ac5237a3191d3804b45d82112b4fe7fe37d0c2
Prin Meng <772062725@qq.com>
2021-05-07 23:25:57 +0800

    Netconf test

--- devnet_http/task_day4/netconf_config_syslog.py
+++ devnet_http/task_day4/netconf_config_syslog.py
@@ -1,15 +1,13 @@
 from yang_xml_maker_template import netconf_config_syslog
 from netconf_request import csr_netconf_config
-from pprint import pprint
 
 
 def conf_log(device_ip, username, password, security_id, host):
-    result = csr_netconf_config(device_ip, username, password, netconf_config_syslog(security_id, host), port='830')
-    print(result)
-    # xmldict = xmltodict.parse(result_xml)
-    # pprint(xmldict)
-    # return xmldict['rpc-reply']['data']['cpu-usage']['cpu-utilization'][monitor_type_use]
+    csr_netconf_config(device_ip, username, password, netconf_config_syslog(security_id, host), port='830')
 
 
 if __name__ == '__main__':
+    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.100')
     conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.88')
+    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.99')
+    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.110')
