e62188d3f41b4fc38472e53065e119d48f4c3fac
Prin Meng <772062725@qq.com>
2021-05-17 12:39:38 +0800

    ansibel and vault

--- devnet_http/task_day4/netconf_config_syslog.py
+++ devnet_http/task_day4/netconf_config_syslog.py
@@ -1,13 +1,15 @@
 from yang_xml_maker_template import netconf_config_syslog
 from netconf_request import csr_netconf_config
+from pprint import pprint
+import xmltodict
 
 
 def conf_log(device_ip, username, password, security_id, host):
-    csr_netconf_config(device_ip, username, password, netconf_config_syslog(security_id, host), port='830')
+    result_xml = csr_netconf_config(device_ip, username, password, netconf_config_syslog(security_id, host), port='830')
+    xmldict = xmltodict.parse(result_xml)
+    pprint(xmldict)
 
 
 if __name__ == '__main__':
     conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.100')
-    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.88')
-    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.99')
-    conf_log('192.168.0.88', 'Prin', 'Cisc0123', '7', '192.168.0.110')
+
---
+++ open_source_software/vault_test/vault_1_init.py
@@ -0,0 +1,14 @@
+import hvac
+
+client = hvac.Client(
+    url='https://192.168.0.166:8200',
+    # token="s.SmqXBFAZPmIIRN0rqpzsPc13",  # 使用权限被控制的令牌
+    token="s.WcsuqUPtTGUQc75oFj4PWc0N",  # 管理员令牌
+    verify="/opt/certs/ca.pem"
+)
+
+# 是否认证
+print(client.is_authenticated())
+
+# 是否解锁
+print(client.sys.is_sealed())
